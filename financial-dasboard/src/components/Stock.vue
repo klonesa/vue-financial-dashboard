6e076b4b140760d66999642cbb7db771dc5c7bbe
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-12-05 02:34:08 +0300

    create navbar

--- financial-dasboard/src/components/Stock.vue
+++ financial-dasboard/src/components/Stock.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="hello">
-    <div class="container">
-      <form class="navbar-form m-2">
-        <div class="input-group no border">
-          <input
-            type="text"
-            value=""
-            class="form-comntrol"
-            placeholder="Search..."
-            v-model="stock"
-          />
-          <button
-            @click="update(stock)"
-            class="btn btn-white btn-round bn-just-icon"
-          >
-            <i class="material-icons ml-3">search</i>
-          </button>
-        </div>
-      </form>
-      <div class="row">
+  <div>
+    <nav class="navbar navbar-dark bg-dark " style="height: 80px">
+      <h1 class="text-white m-2">Financial Dashboard</h1>
+      <div class="btn-group m-3" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-outline-secondary text-white">Admin</button>
+        <button type="button" class="btn btn-outline-secondary text-white">Guest</button>
+      </div>
+    </nav>
+    <div class="hello">
+      <div class="container">
+        <form class="navbar-form m-2">
+          <div class="input-group no border">
+            <input
+              type="text"
+              value=""
+              class="form-comntrol"
+              placeholder="Search..."
+              v-model="stock"
+            />
+            <button
+              @click="update(stock)"
+              class="btn btn-white btn-round bn-just-icon"
+            >
+              <i class="material-icons ml-3">search</i>
+            </button>
+          </div>
+        </form>
+        <!-- <div class="row">
         <div class="col-md-3">
           <div class="btn-group" role="group" aria-label="Basic example">
             <button type="button" class="btn btn-outline-secondary">Daily</button>
@@ -26,13 +34,14 @@
             <button type="button" class="btn btn-outline-secondary">Montly</button>
           </div>
         </div>
+      </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "Stock",
   props: {
@@ -41,24 +50,26 @@ export default {
   data() {
     return {
       stock: "",
-      beta: '',
-      infoCompany: ''
+      infoCompany: "",
     };
   },
   methods: {
     update(stock) {
       // console.log(stock);
-      this.getInfo(stock)
+      this.getInfo(stock);
     },
-    getInfo(stock){
-      console.log(stock)
-      axios.get('https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=e77bafdd8b20ee59904e279c50368683')
-      .then(res => {
-        this.infoCompany = res.data.profile
-      })
-      .catch(err => console.log(err))
-      console.log(this.infoCompany)
-    }
+    getInfo(stock) {
+      console.log(stock);
+      axios
+        .get(
+          "https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=e77bafdd8b20ee59904e279c50368683"
+        )
+        .then((res) => {
+          this.infoCompany = res.data.profile;
+        })
+        .catch((err) => console.log(err));
+      console.log(this.infoCompany);
+    },
   },
 };
 </script>
@@ -79,6 +90,4 @@ li {
 a {
   color: #42b983;
 }
-
-
 </style>
--- financial-dasboard/src/views/Home.vue
+++ financial-dasboard/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div >
-    
     <Stock/>
   </div>
 </template>
