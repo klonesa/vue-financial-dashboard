1f09910bf4ac35fd71d53569007273919f2b176a
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-12-04 18:18:30 +0300

    fix: index.html

---
+++ financial-dasboard/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdList)
+Vue.use(MdIcon)
+Vue.use(MdBadge)
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
